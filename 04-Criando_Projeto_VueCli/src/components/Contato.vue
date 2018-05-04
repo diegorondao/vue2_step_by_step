281de05afc97592b028eec1861855c32c82d4d52
diegorondao <diegorondao86@gmail.com>
2018-05-03 22:57:57 -0300

    Finalizando a seção 5

---
+++ 04-Criando_Projeto_VueCli/src/components/Contato.vue
@@ -0,0 +1,33 @@
+<template>
+  <p>Telefone da empresa {{msg}}</p>
+
+</template>
+
+<script>
+export default {
+  name: 'Contato',
+  data () {
+    return {
+      msg: '(11) 9456213521'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- 04-Criando_Projeto_VueCli/src/router/index.js
+++ 04-Criando_Projeto_VueCli/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Sobre from '@/components/Sobre'
+import Contato from '@/components/Contato'
 
 Vue.use(Router)
 
@@ -10,6 +12,16 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/sobre',
+      name: 'Sobre',
+      component: Sobre
+    },
+    {
+      path: '/contato',
+      name: 'Contato',
+      component: Contato
     }
   ]
 })
