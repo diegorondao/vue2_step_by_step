3b96f1732f4e8a69240bf0c8b2dfa1c14ba8bc6e
diegorondao <diegorondao86@gmail.com>
2018-05-07 22:49:39 -0300

    finalizado a seção Seção 8 Vuejs Material Design

--- 04-Criando_Projeto_VueCli/src/components/Contato.vue
+++ 04-Criando_Projeto_VueCli/src/components/Contato.vue
@@ -6,19 +6,46 @@
         <md-card-content>
             Example criação de um Card
         </md-card-content>
-        <md-button class="md-raised md-primary">TABLE</md-button>
-        <md-button class="md-raised md-primary">CLEAR</md-button>
+        <md-button class="md-raised md-primary" @click="table()">TABLE</md-button>
+        <md-button class="md-raised md-primary" @click="limpar()">CLEAR</md-button>
+
+        <md-table>    
+                <md-table-row>
+                    <md-table-cell>PRIMEIRO NOME</md-table-cell>
+                    <md-table-cell>SEGUNDO NOME </md-table-cell>
+                    <md-table-cell>E-MAIL</md-table-cell>
+                </md-table-row>
+            <md-table-row v-for="contato in contatos">
+                <md-table-cell> {{contato.nome}} </md-table-cell>
+                <md-table-cell> {{contato.sobrenome}} </md-table-cell>
+                <md-table-cell> {{contato.email}} </md-table-cell>
+            </md-table-row>
+        </md-table>    
     </md-card>
 </template>
 
 <script>
 export default {
   name: 'Contato',
-  data () {
+  data: function () {
     return {
-      msg: '(11) 9456213521'
+      contatos: [
+        { nome: 'User 01', sobrenome:'User 01', email: 'us1@mail.com'},
+        { nome: 'User 02', sobrenome:'User 02', email: 'us2@mail.com'}
+      ]
+    }
+  },
+  methods:{
+    table: function (){ 
+        this.contatos.push({ nome: 'Sebastian', sobrenome:'Escheweiler', email: 'sebastiab@mail.com'})
+        this.contatos.push({ nome: 'Bill', sobrenome:'brazil', email: 'bill@mail.com'})
+        this.contatos.push({ nome: 'Ann', sobrenome:'stheinenr', email: 'ann@mail.com'})
+    },
+    limpar: function () {
+        this.contatos.splice(0, this.contatos.length)
     }
   }
+
 }
 </script>
 
