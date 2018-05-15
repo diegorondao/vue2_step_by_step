e880cecbeb8b95974b0ded9ec850c07115ba1e87
diegorondao <diegorondao86@gmail.com>
2018-05-14 21:40:28 -0300

    consumindo Api com Vue and Axios

---
+++ 05-Axios/src/components/AxiosApi.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <ul v-if="posts && posts.length">
+        <li v-for="post in posts">
+            <p><strong>Titúlo: {{post.title}}</strong></p>
+            <p>{{post.body}}</p>
+        </li>
+    </ul>
+
+  </div>
+</template>
+<script>
+
+import axios from 'axios'
+
+export default {
+
+    name: 'AxiosApi',
+  
+    data () {
+        return {
+            posts: [],
+            errors: [],
+        }
+    
+    },
+
+    created(){
+        axios.get('https://jsonplaceholder.typicode.com/posts').then(response =>{
+
+            this.posts = response.data
+        })
+        .catch( e => {
+            this.errors.push(e)
+        })
+    }
+}
+</script>
